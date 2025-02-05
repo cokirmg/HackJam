e42fb3f157c01371d4680dd182687a1e3f7caf05
cokirmg <100722843+cokirmg@users.noreply.github.com>
2025-02-05 18:42:49 +0100

    First push

---
+++ Source/HackJamProject/HackJamProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HackJamProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HackJamProject, "HackJamProject" );
